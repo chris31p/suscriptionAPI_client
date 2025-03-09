f5273112b8995a458c5ed1bbc3f6e18a3d54c5f3
Christopher <chriuser.email christopher.a31@hotmail.comgit config --global user.name Chris>
2025-03-08 21:18:46 -0500

    Fix: corregir formato del codigo de pedido

--- src/pages/MyOrders.jsx
+++ src/pages/MyOrders.jsx
@@ -18,7 +18,7 @@ const MyOrders = () => {
             key={order._id + index + "order"}
             className="flex-auto rounded p-4 text-sm"
           >
-            <p className="font-semibold border-2 max-w-screen-sm">Pedido No: {order?.orderId}</p>
+            <p className="font-semibold border-2 max-w-screen-sm">Pedido No: {`ORD-${order?.orderId?.slice(0, 6)}`}</p>
             <div className="flex gap-5 bg-green-50 border-1 max-w-screen-sm">
               <img src={order.product_details.image[0]} className="w-28 h-24" />
               <p className="py-4 font-medium">{order.product_details.name}</p>
